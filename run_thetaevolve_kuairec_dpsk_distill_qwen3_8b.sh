60602138cac865576580db6e52f634723daa6f45
Your Name <minghaoyan_google_com@instance-20260313-204308.europe-west4-a.c.tianxinwei-intern-2025.internal>
2026-04-27 21:58:28 +0000

    update log

--- run_thetaevolve_kuairec_dpsk_distill_qwen3_8b.sh
+++ run_thetaevolve_kuairec_dpsk_distill_qwen3_8b.sh
@@ -8,4 +8,9 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export TASK="kuairec"
 export SMALL_MODEL_NAME="${SMALL_MODEL_NAME:-dpsk_distill_qwen3_8b}"
 
+export THETAEVOLVE_TRAIN_SEQ_LENGTH=2048
+export THETAEVOLVE_MAX_POSITION_EMBEDDINGS=2048
+export THETAEVOLVE_MAX_TOKENS_PER_GPU=1024
+export THETAEVOLVE_LOG_PROBS_MAX_TOKENS_PER_GPU=1024
+
 bash "${ROOT_DIR}/run_thetaevolve_task.sh"
